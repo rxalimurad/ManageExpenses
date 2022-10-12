725e46082f5aa80fca91dcbd37ee24996b400153
alimurad <rxalimurad@gmail.com>
2022-10-12 14:43:27 +0500

    Initial Commit

---
+++ ManageExpensesUITests/ManageExpensesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ManageExpensesUITestsLaunchTests.swift
+//  ManageExpensesUITests
+//
+//  Created by Ali Murad on 12/10/2022.
+//
+
+import XCTest
+
+class ManageExpensesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
