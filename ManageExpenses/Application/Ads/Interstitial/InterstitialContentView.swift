5f870a421877a6bdf2e310eb98e6ef553458d70b
alimurad <amurad@i2cinc.com>
2024-07-24 17:24:37 +0500

    --

--- ManageExpenses/Application/Ads/Interstitial/InterstitialContentView.swift
+++ ManageExpenses/Application/Ads/Interstitial/InterstitialContentView.swift
@@ -2,7 +2,6 @@ import GoogleMobileAds
 import SwiftUI
 
 struct InterstitialContentView: View {
-  @StateObject private var countdownTimer = CountdownTimer()
   @State private var showGameOverAlert = false
   private let viewModel = InterstitialViewModel()
   let navigationTitle: String
@@ -14,51 +13,24 @@ struct InterstitialContentView: View {
 
       Spacer()
 
-      Text(countdownTimer.isComplete ? "You lose!" : "\(countdownTimer.timeLeft)")
-        .font(.title2)
 
       Button("Play Again") {
         startNewGame()
       }
       .font(.title2)
-      .opacity(countdownTimer.isComplete ? 1 : 0)
 
       Spacer()
     }
-    .onAppear {
-      if !countdownTimer.isComplete {
-        startNewGame()
-      }
-    }
-    .onDisappear {
-      countdownTimer.pause()
-    }
-    .onChange(of: countdownTimer.isComplete) { newValue in
-      showGameOverAlert = newValue
-    }
-    .alert(isPresented: $showGameOverAlert) {
-      Alert(
-        title: Text("Game Over"),
-        message: Text("You lasted \(countdownTimer.countdownTime) seconds"),
-        dismissButton: .cancel(
-          Text("OK"),
-          action: {
-            viewModel.showAd()
-          }))
-    }
+    
+  
+
     .navigationTitle(navigationTitle)
   }
 
   private func startNewGame() {
-    countdownTimer.start()
+//    countdownTimer.start()
     Task {
       await viewModel.loadAd()
     }
-  }
-}
-
-struct InterstitialContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    InterstitialContentView(navigationTitle: "Interstitial")
   }
 }
